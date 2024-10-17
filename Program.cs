4a47872119ac0f20eca94d7e5402168ab228fe4c
Elinaphinphat <enaphinphat@wsutech.edu>
2024-10-17 17:04:45 -0500

    Adding all project files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a String: ");
+        string inputChar = Console.ReadLine().ToLower();
+        var counter = inputChar
+                        .Where(char.IsLetter)
+                        .ToLookup(c => c);
+
+        foreach (var c in counter)
+        {
+            Console.WriteLine("Character: {0} Count: {1}", c.Key, counter[c.Key].Count());
+        }
+        }
+    }   
